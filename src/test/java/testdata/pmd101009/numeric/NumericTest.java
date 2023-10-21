35ded0c4cd62144cc75ef9465b07cab8cc4194dd
olyutorskii <5801520+olyutorskii@users.noreply.github.com>
2023-10-21 13:18:07 +0900

    Split expected output datas between JDK18 and JDK19

--- src/test/java/testdata/pmd101009/numeric/NumericTest.java
+++ src/test/java/testdata/pmd101009/numeric/NumericTest.java
@@ -4,8 +4,11 @@
 package testdata.pmd101009.numeric;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledForJreRange;
 
 import static testdata.CnvAssert.*;
+import static org.junit.jupiter.api.condition.JRE.JAVA_18;
+import static org.junit.jupiter.api.condition.JRE.JAVA_19;
 
 /**
  *
@@ -20,9 +23,18 @@ public class NumericTest {
     }
 
     @Test
+    @EnabledForJreRange(max = JAVA_18)
     public void pmd2xml() throws Exception{
         System.out.println("pmd2xml");
         assertPmd2Xml(THISCLASS, "numeric.pmd", "result.xml");
+        return;
+    }
+
+    @Test
+    @EnabledForJreRange(min = JAVA_19)
+    public void pmd2xml19Later() throws Exception{
+        System.out.println("pmd2xml");
+        assertPmd2Xml(THISCLASS, "numeric.pmd", "result19Later.xml");
         return;
     }
 
